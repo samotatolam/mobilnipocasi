1f1be241e12688762806d5025ea92d759e231d17
samotatolam <44637406+samotatolam@users.noreply.github.com>
2018-11-10 14:22:28 +0100

    oprava pocasi.ts

--- src/providers/pocasi/pocasi.ts
+++ src/providers/pocasi/pocasi.ts
@@ -4,18 +4,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class PocasiProvider {
   apiKey = '4b1f6959f47473915fb1e08d553cee60';
+  town = 'Paris';
   url;
 
   constructor(public http: HttpClient) {
     console.log('Hello PocasiProvider Provider');
-    //this.url = 'api.openweathermap.org/data/2.5/weather?q=London&APPID='+this.apiKey+'';
-    //this.url = 'http://api.openweathermap.org/data/2.5/weather?q=Calgary,ca&appid=4b1f6959f47473915fb1e08d553cee60';
-    this.url = 'http://api.openweathermap.org/data/2.5/weather?q=Calgary,ca&appid='+this.apiKey+'';
+    this.url = 'http://api.openweathermap.org/data/2.5/weather?q='+this.town+'&appid='+this.apiKey+'';
   }
 
   getPocasi(city){
-    //return this.http.get(this.url+'/'+city+'.json');
-    return this.http.get(this.url+'/'+city);
+    return this.http.get(this.url);
   }
 
 }
