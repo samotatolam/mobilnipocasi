892bb36bc841b497f451fbd8339d7d6530a7780e
samotatolam <44637406+samotatolam@users.noreply.github.com>
2018-11-10 11:52:37 +0100

    pridan HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -12,6 +13,7 @@ import {Nastaveni} from '../pages/nastaveni/nastaveni';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { PocasiProvider } from '../providers/pocasi/pocasi';
+
 
 @NgModule({
   declarations: [
@@ -24,6 +26,7 @@ import { PocasiProvider } from '../providers/pocasi/pocasi';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {PocasiProvider} from '../../providers/pocasi/pocasi';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  pocasi:any;
+  location:{
+    city:string
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private pocasiProvider:PocasiProvider) {
 
+  }
+
+  ionViewWillEnter(){
+    this.location = {
+      city: 'Miami' 
+    }
   }
 
 }
